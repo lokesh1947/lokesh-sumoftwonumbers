a0877060e4a14a3b0e51d9517a9f8bd694c971e9
Ragula Karthik <121540538+ragulakarthik04@users.noreply.github.com>
2023-01-04 11:43:28 +0530

    Update classes.cpp
    keep braces in a new line so that we can understand code well

--- classes.cpp
+++ classes.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
-class Basic_Info{
+class Basic_Info
+{
 	public:
 		string name,gender;
 		int rollno;
-		void getdata(){
+		void getdata()
+		{
 			cout<<"enter name:";
 			cin>>name;
 			cout<<"enter roll no:";
@@ -12,25 +14,30 @@ class Basic_Info{
 			cout<<"enter gender:";
 			cin>>gender;
 		}
-		void display(){
+		void display()
+		{
 		    cout<<rollno<<","<<name<<","<<gender<<",";
 		}
 };
-class Physical_Fit{
+class Physical_Fit
+{
 	public:
-    int height,weight;
-	void getdata(){
+	int height,weight;
+	void getdata()
+	{
 		 cout<<"enter height:";
 		 cin>>height;
 		 cout<<"enter weight:";
 		 cin>>weight;
 	}
-	void display(){
+	void display()
+	{
 		 cout<<height<<","<<weight;
 	}
 };
 
-int main(){
+int main()
+{
 	Basic_Info obj1,obj2;
 	Physical_Fit S1,S2;
 
